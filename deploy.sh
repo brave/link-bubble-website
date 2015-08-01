b448c56e7d780e893888e9418ba24388f7643d21
Kevin Grandon <kevingrandon@yahoo.com>
2015-08-01 15:51:00 -0700

    Add simple deploy script.

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Deploys to github pages.
+
+git checkout -b gh-pages
+staticjinja build --srcpath=$(pwd)/src/templates
+git add *
+git commit -m "Deployed to Github Pages"
+git push origin :gh-pages
+#git subtree push --prefix src origin gh-pages
+git push origin gh-pages
+git checkout master
+git reset --hard HEAD
+git branch -D gh-pages
